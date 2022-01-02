6ff3587b00fd24c871eddcc6d9fa3bccf5a68fe8
Timothy Otieno <timothyking094@gmail.com>
2022-01-02 21:34:55 +0300

    add functionality to toggle portfolio-title

--- app.js
+++ app.js
@@ -4,13 +4,27 @@ $(
     function(){
         toggleWhatWeDo('.product-management-icon', '.product-management-content');
         toggleWhatWeDo('.product-management-content', '.product-management-icon');
-        toggleWhatWeDo('.development-icon', '.development-content')
-        toggleWhatWeDo('.development-content', '.development-icon')
-        toggleWhatWeDo('.design-icon','.design-content')
-        toggleWhatWeDo('.design-content', '.design-icon')
+        toggleWhatWeDo('.development-icon', '.development-content');
+        toggleWhatWeDo('.development-content', '.development-icon');
+        toggleWhatWeDo('.design-icon','.design-content');
+        toggleWhatWeDo('.design-content', '.design-icon');
+
+
+        /** Portfolio Hover Effect */
+        let portfolioItems = $('.portfolio-items-container').children();
+        portfolioItems.each(function(){
+            $(this).hover(function mouseEnter(){
+                let title = $(this).children()[1];
+                $(title).removeClass('d-none').addClass('toggle-portfolio-title')
+            }, function mouseLeave(){
+                let title = $(this).children()[1]
+                $(title).addClass('d-none')
+            })
+        })
     }
 )
 
+/** Toggle What we do Items */
 function toggleWhatWeDo(class1, class2){
     $(class1).click(function(){
         $(this).addClass('d-none');
